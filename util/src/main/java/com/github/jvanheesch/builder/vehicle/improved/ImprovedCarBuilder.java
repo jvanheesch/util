276fa3b30b81ebe7f40153118eaeef2e15e9b7fb
jvanheesch <joris.vanheesch@ae.be>
2019-06-22 15:50:08 +0200

    Improved but fundamentally broken it seems - see next commit.

--- util/src/main/java/com/github/jvanheesch/builder/vehicle/improved/ImprovedCarBuilder.java
+++ util/src/main/java/com/github/jvanheesch/builder/vehicle/improved/ImprovedCarBuilder.java
@@ -6,30 +6,33 @@ import com.github.jvanheesch.builder.compiletime.BuilderOption2;
 
 public class ImprovedCarBuilder<
         BEIGE_AND_RED extends Red<AUDI_AND_BMW> & Beige<AUDI_AND_BMW> & BaseBuilder<AUDI_AND_BMW>,
-        AUDI_AND_BMW extends Audi<BEIGE_AND_RED> & Bmw<BEIGE_AND_RED> & BaseBuilder<AUDI_AND_BMW>> implements
-        Audi<BEIGE_AND_RED>,
-        Bmw<AUDI_AND_BMW>,
-        BaseBuilder<ImprovedCarBuilder<BEIGE_AND_RED, AUDI_AND_BMW>> {
+        BEIGE_AND_RED_TERMINAL extends Red<Void> & Beige<Void> & BaseBuilder<AUDI_AND_BMW>,
+        AUDI_AND_BMW extends Audi<BEIGE_AND_RED> & Bmw<BEIGE_AND_RED> & BaseBuilder<AUDI_AND_BMW>,
+        AUDI_AND_BMW_TERMINAL extends Audi<Void> & Bmw<Void> & BaseBuilder<AUDI_AND_BMW>>
+        implements
+        Audi<BEIGE_AND_RED_TERMINAL>,
+        Bmw<BEIGE_AND_RED_TERMINAL>,
+        BaseBuilder<ImprovedCarBuilder<BEIGE_AND_RED, BEIGE_AND_RED_TERMINAL, AUDI_AND_BMW, AUDI_AND_BMW_TERMINAL>> {
 
     @Override
-    public <R extends BEIGE_AND_RED> R audi() {
+    public <R extends BEIGE_AND_RED_TERMINAL> R audi() {
         System.out.println("SomeBuilder.audi");
         return (R) this;
     }
 
     @Override
-    public ImprovedCarBuilder<BEIGE_AND_RED, AUDI_AND_BMW> doStuff() {
-        return this;
-    }
-
-    @Override
-    public <R extends AUDI_AND_BMW> R bmw() {
+    public <R extends BEIGE_AND_RED_TERMINAL> R bmw() {
         System.out.println("SomeBuilder.bmw");
         return (R) this;
     }
 
+    @Override
+    public ImprovedCarBuilder<BEIGE_AND_RED, BEIGE_AND_RED_TERMINAL, AUDI_AND_BMW, AUDI_AND_BMW_TERMINAL> doStuff() {
+        return this;
+    }
+
     public static void main(String[] args) {
-        ImprovedCarBuilder<?, ?> builder = new ImprovedCarBuilder<>();
+        ImprovedCarBuilder<?, ?, ?, ?> builder = new ImprovedCarBuilder<>();
 
         BuilderOption1<?> o1 = builder.audi();
         // o1.option1().option2();
@@ -42,11 +45,11 @@ public class ImprovedCarBuilder<
                 //. .audi() does not compile
                 .red()
                 // .red() does not compile
-                .audi() // -> we want this NOT to compile !!
-                .red()
-                .doStuff()
-                // .option2() // does not compile
-                .audi()
+//                .audi() // -> we want this NOT to compile !!
+//                .red()
+//                .doStuff()
+//                // .option2() // does not compile
+//                .audi()
         ;
     }
 }
